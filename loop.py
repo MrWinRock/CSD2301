de523279fc9dec20d0133b8728dd1b931b268823
MrWinRock <mrwinrock11@gmail.com>
2024-06-28 21:28:10 +0700

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,3 +1,6 @@
+import random
+
+
 def loop1():
     for _ in range(100):
         print("Pharthiwath")
@@ -15,12 +18,12 @@ def loop3():
 
 def loop4():
     for i in range(8, 90, 3):
-        print(i)
+        print(i, end=", ")
 
 
 def loop5():
     for i in range(100, 0, -2):
-        print(i)
+        print(i, end=", ")
 
 
 def loop6():
@@ -42,14 +45,68 @@ def loop7():
         print(user)
 
 
+def loop8():
+    n = int(input("Amount if Fibbonacci: "))
+    a, b = 1, 1
+    for _ in range(n):
+        print(a, end=", ")
+        a, b = b, a + b  # a = b, b = a + b
+
+
+def loop9():
+    h = int(input("Height: "))
+    for i in range(1, h + 1):
+        print('*' * i)
+
+
+def loop10():
+    start = 3
+    end = 6
+    for _ in range(50):
+        print(random.randint(start, end), end=", ")
+
+
+def loop11():
+    x, y = random.randint(1, 50), random.randint(2, 5)
+    print(x**y)
+
+
+def loop12():
+    n = random.randint(1, 10)
+    for _ in range(n):
+        print("Pharthiwath")
+
+
+def loop13():
+    for i in range(1, 11):
+        for j in range(1, i + 1):
+            print(i * j, end=" ")
+        print()
+
+
+def loop14():
+    for i in range(1, 6):
+        for j in range(i, 6):
+            print(j, end=" ")
+        print()
+
+
 def main():
-    # loop1()
+    "Uncomment each line and comment other lines to run the code"
+    loop1()
     # loop2()
     # loop3()
     # loop4()
     # loop5()
-    loop6()
+    # loop6()
     # loop7()
+    # loop8()
+    # loop9()
+    # loop10()
+    # loop11()
+    # loop12()
+    # loop13()
+    # loop14()
 
 
 main()
